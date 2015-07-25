570f03e7f40ac84f594222cc0cc39de6f17dec0a
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-07-24 23:53:55 -0500

    Spaces, ah!

--- config/configuration.php
+++ config/configuration.php
@@ -3,14 +3,14 @@
 return [
 
     /*
-	|--------------------------------------------------------------------------
-	| Package Authors
-	|--------------------------------------------------------------------------
-	|
-	| When you create new packages NewUp will automatically add the
+    |--------------------------------------------------------------------------
+    | Package Authors
+    |--------------------------------------------------------------------------
+    |
+    | When you create new packages NewUp will automatically add the
     | following authors to the generated "composer.json" file.
     | You can add as many authors as you like to the list.
-	|
+    |
     */
     'authors' => [
         [
@@ -20,30 +20,30 @@ return [
     ],
 
     /*
-	|--------------------------------------------------------------------------
-	| Package License
-	|--------------------------------------------------------------------------
-	|
-	| When you create new packages NewUp will automatically set the
+    |--------------------------------------------------------------------------
+    | Package License
+    |--------------------------------------------------------------------------
+    |
+    | When you create new packages NewUp will automatically set the
     | license for the newly created package template. You should
     | set the following license to your preferred license.
-	|
+    |
     */
     'license' => 'MIT',
 
     /*
-	|--------------------------------------------------------------------------
-	| Strict Composer Values
-	|--------------------------------------------------------------------------
-	|
-	| By default, the NewUp utility is pretty strict when validating
+    |--------------------------------------------------------------------------
+    | Strict Composer Values
+    |--------------------------------------------------------------------------
+    |
+    | By default, the NewUp utility is pretty strict when validating
     | package authors and licenses. If you set the "authors" and
     | "license" config entries, you don't have to worry what
     | this setting does. If you didn't, NewUp will throw
     | various errors. Set this to 'false' to suppress
     | these error messages. You should really set
     | the above configuration entries, however.
-	|
+    |
     */
     'strictComposerValues' => true,
 
